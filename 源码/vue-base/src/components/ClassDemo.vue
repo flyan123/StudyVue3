5bde4f079192b1dc70caacf88b1767353b24d039
flyan123 <2495365901@qq.com>
2024-04-04 23:43:55 +0800

    add 0404

---
+++ 源码/vue-base/src/components/ClassDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <p :class="{}">Class样式绑定</p>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                myClass:'demo'
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
